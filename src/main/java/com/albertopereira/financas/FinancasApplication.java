7c3924d62f101476aca512af269acb811bf6f427
Alberto Pereira <alberto.pereira.gyn@gmail.com>
2022-01-28 15:46:33 -0300

    backend finalizado

--- src/main/java/com/albertopereira/financas/FinancasApplication.java
+++ src/main/java/com/albertopereira/financas/FinancasApplication.java
@@ -2,10 +2,14 @@ package com.albertopereira.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinancasApplication {
-
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
 	}
--- src/main/java/com/albertopereira/financas/api/resource/LancamentoController.java
+++ src/main/java/com/albertopereira/financas/api/resource/LancamentoController.java
@@ -58,6 +58,12 @@ public class LancamentoController {
 		return ResponseEntity.ok(lancamentos);
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
+	
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody LancamentoDTO dto) {
 		try {
@@ -113,6 +119,18 @@ public class LancamentoController {
 			
 		}).orElseGet( () -> 
 		new ResponseEntity("Lançamento não encontrado na base de dados", HttpStatus.BAD_REQUEST));
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder().id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
